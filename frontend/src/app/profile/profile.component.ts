15a9d045628076a3aa98291149a5989b54a636ca
Kaushik Samanta <kaushiksamanta23@gmail.com>
2018-08-25 14:08:48 -0400

    landing page changed

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-profile',
@@ -7,8 +8,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  profileData = {
+    country: '',
+    city: '',
+    email: '',
+    password: '',
+    fullName: '',
+    selectedDaysId: '',
+    volunteeringDayMin: '',
+    volunteeringDayMax: ''
+  };
 
-  ngOnInit() { }
+  causes:any = [];
+  daysToVolunteer:any = [
+    {
+      min: 1,
+      max: 5,
+      id: 1
+    },
+    {
+      min: 5,
+      max: 10,
+      id: 2
+    },
+    {
+      min: 10,
+      max: 15,
+      id: 3
+    },
+    {
+      min: 15,
+      max: 20,
+      id: 4
+    }
+  ];
 
+  constructor(private http: Http) { }
+
+  ngOnInit() { 
+    this.http.get('http://localhost:3000/causes/').subscribe(res => {
+      const body = JSON.parse(res['_body']);
+      body.data.forEach((element: any) => {
+        element.checked = false;
+      });  
+      this.causes = body.data;
+    });
+  }
+
+  setDaysToVolunteer(id: any) {
+    this.daysToVolunteer.forEach((element: any) => {
+      if(element.id === id) {
+        this.profileData.volunteeringDayMin = element.min;
+        this.profileData.volunteeringDayMax = element.max;
+      }
+    });
+  }
+
+  submit() {
+    this.setDaysToVolunteer(this.profileData.selectedDaysId);
+    const body = {
+      fullName: this.profileData.fullName,
+      country: this.profileData.country,
+      city: this.profileData.city,
+      email: this.profileData.email,
+      password: this.profileData.password,
+      volunteeringDayMin: this.profileData.volunteeringDayMin,
+      volunteeringDayMax: this.profileData.volunteeringDayMax
+    };
+    this.http.post('http://localhost:3000/users/', body).subscribe(res => {
+      console.log(res.ok);
+    });
+  }
 }
--- frontend/src/app/profile/profile.module.ts
+++ frontend/src/app/profile/profile.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
-
+import { HttpModule } from '@angular/http';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     TranslateModule,
+    HttpModule,
+    FormsModule,
     ProfileRoutingModule
   ],
   declarations: [
